f8a270063c9494d63202dd288a1099f18b57e92d
Atharva-gt <guptaatharva098@gmail.com>
2021-10-28 16:18:29 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 5000 || process.env.PORT;
 const cors = require("cors");
 const userRouter = require("./routers/userRouter");
 const fileRouter = require("./routers/fileRouter");
