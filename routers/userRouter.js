8a38abdf8181d00d92c20d9d165f4a346d73486d
Atharva-gt <guptaatharva098@gmail.com>
2021-10-31 23:37:35 +0530

    updated

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -17,4 +17,37 @@ router.post('/add', (req, res) => {
 
         })
 })
+router.post('/login', (req, res) => {
+
+    const data = req.body;
+    console.log(data);
+  
+     Model.findOne({email:data.email})
+        .then((db_data) => {
+            if(db_data)
+            {
+                if(data.password == db_data.password)
+                {
+                    console.log('Login Success');
+                    res.status(200).json(db_data);
+                    return
+                }
+                else{
+                    console.log('User Not Found');
+                    res.status(300).json({ message: 'error' });
+                }
+            }
+            else{
+                console.log('User Not Found');
+                res.status(300).json({ message: 'error' });
+            }
+          
+        }
+        )
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json(err);
+  
+        })
+  })
 module.exports = router;
